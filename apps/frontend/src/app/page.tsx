f6f5efa9b31b4407437cacd7b8658638435eb712
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-09-07 13:57:22 -0300

    fix: ajusta responsividade

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -6,10 +6,10 @@ const luckiestGuy = Luckiest_Guy({ subsets: ['latin'], weight:'400' });
 export default function Home() {
   return (
     <div
-      className="h-screen bg-cover bg-center"
+      className="h-screen bg-cover bg-center w-screen"
       style={{ backgroundImage: `url(${fundo.src})` }}
     >
-      <div className="h-full flex flex-col items-center text-black">
+      <div className="h-full flex flex-col items-center text-black w-full">
         <h1 className="text-4xl font-bold m-4" style={luckiestGuy.style}>JANKENPON</h1>
         <Game />
       </div>
